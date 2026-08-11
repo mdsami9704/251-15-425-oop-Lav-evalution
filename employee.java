d662d73436103c29edfbc5a3f3b60ce46b0e2e62
Md Sami <307691876+mdsami9704@users.noreply.github.com>
2026-08-11 19:49:24 +0600

    Create Employee class with constructor and main method
    Fix constructor syntax and add main method.

---
+++ employee.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.employee;
+
+import java.util.*; 
+
+
+public class Employee {
+    String name;
+    String department;
+    Double salary;
+    Employee(String name,String department,double salary);
+    this.name = name;
+    this.department = department;
+    this.salary = salary;
+    system.out.println{
+
+    public static void main(String[] args) {
+        Scannersc=new scanner(system.in);
+    }
+        System.out.println("");
+    }
+}
